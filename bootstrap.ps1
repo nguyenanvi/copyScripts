c07b53fab2ccb4b65c746884a77c469dc5b773c9
Nguyễn An Vĩ <100780275+nguyenanvi@users.noreply.github.com>
2025-10-19 14:36:01 +0000

    fix bootstrap spelling

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,9 +1,9 @@
-$repoUrl = "https://github.com/nguyenanvi/copyScript/archive/refs/heads/main.zip"
-$zipPath = "$env:TEMP\copyScript.zip"
-$extractPath = "$env:TEMP\copyScript"
+$repoUrl = "https://github.com/nguyenanvi/copyScripts/archive/refs/heads/main.zip"
+$zipPath = "$env:TEMP\copyScripts.zip"
+$extractPath = "$env:TEMP\copyScripts"
 
 Invoke-WebRequest -Uri $repoUrl -OutFile $zipPath
 Expand-Archive -Path $zipPath -DestinationPath $extractPath -Force
 
-Set-Location "$extractPath\copyScript-main"
+Set-Location "$extractPath\copyScripts-main"
 Start-Process ".\RUN.cmd"
